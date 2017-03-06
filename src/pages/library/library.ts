5f404dad0ba135cab0707ccb3e9b8b4039f3aac2
mahmudul <mahmudul@mahmuduls-Air.fios-router.home>
2017-03-05 23:06:30 -0500

    library page

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,22 +1,15 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../data/quote.interface';
+import quoteData from '../../data/quotes';
 
-/*
-  Generated class for the Library page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html'
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
+  ngOnInit() {
+    this.quoteCollection = quoteData;
   }
-
 }
